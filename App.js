60807f5a2d3a78cb1d242002213a3dbf6ec2cc9f
Maliha <135051893+malih753@users.noreply.github.com>
2023-08-20 19:53:38 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
+import Carousel from './src/components/Carousel';
+import Navbars from './src/navigation/Navbars';
+
+export default function App() {
+  return <Navbars />;
+  // <AppNavigator />;
+}
